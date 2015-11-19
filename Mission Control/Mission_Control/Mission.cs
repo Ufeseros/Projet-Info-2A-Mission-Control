d12f05adcfec867f33ede407720b82419681ec30
Ufeseros <pariffaud@hotmail.fr>
2015-11-19 12:42:38 +0100

    update
    update

---
+++ Mission Control/Mission_Control/Mission.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mission_Control
+{
+    class Mission
+    {
+        private int Durée;
+        private string carte;
+ 
+    }
+}
